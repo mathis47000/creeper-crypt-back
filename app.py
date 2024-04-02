b9ebbc76504c07792499d9ab659951ab8659b8a1
mathis47000 <mathisbescond3@gmail.com>
2024-04-02 22:30:03 +0200

    create room (password)

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 from flask import Flask
-from flask_socketio import SocketIO, emit
+import uuid
+from flask_socketio import SocketIO, emit, join_room
 from db import create_connection, create_table, insert_message, get_messages
     
 app = Flask(__name__)
-db = create_connection()
+# db = create_connection()
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, cors_allowed_origins='*')
 
 @socketio.on('message')
 def handle_message(data):
-
-    print('received message: ' + data)
     emit('message', data, broadcast=True)
 
 @socketio.on('connect')
@@ -20,6 +19,15 @@ def handle_connect():
 @socketio.on('disconnect')
 def handle_disconnect():
     print('disconnected')
+    
+@socketio.on('createroom')
+def on_create_room(data):
+    roomName = data['roomName']
+    password = data['password']
+    # create room
+    url = str(uuid.uuid4())
+    join_room(url)
+    return {'url': url}
 
 if __name__ == '__main__':
     socketio.run(app)
