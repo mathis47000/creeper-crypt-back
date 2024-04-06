2cff3e296ded5bbc1404c30c4bbcaf129f8c5c57
mathis47000 <mathisbescond3@gmail.com>
2024-04-06 18:26:58 +0200

    load messages and rename url by id

--- model.py
+++ model.py
@@ -2,9 +2,9 @@ import uuid
 
 class Room:
     def __init__(self, roomName, password):
+        self.id = str(uuid.uuid4())
         self.roomName = roomName
         self.password = password
-        self.url = str(uuid.uuid4())
         self.messages = []
         
     def add_message(self, message):
